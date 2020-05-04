9047e87094af927b0a884c980413c0960c07125d
Izabela Paderewska <izabela.paderewska@hycom.pl>
2020-05-04 13:57:28 +0200

    Adding basic methods in TaskController

--- src/main/java/io/github/iza/todoapp/controller/TaskController.java
+++ src/main/java/io/github/iza/todoapp/controller/TaskController.java
@@ -4,12 +4,15 @@ import io.github.iza.todoapp.model.Task;
 import io.github.iza.todoapp.model.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
-@RepositoryRestController
+@RestController
 public class TaskController {
 
     public static final Logger logger = LoggerFactory.getLogger(TaskRepository.class);
@@ -18,8 +21,39 @@ public class TaskController {
     public TaskController(final TaskRepository repository) {
         this.repository = repository;
     }
+
+    @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
     ResponseEntity<List<Task>> readAllTasks() {
         logger.warn("Exposing all the tasks!");
         return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping("/tasks")
+    ResponseEntity<List<Task>> readAllTasks(Pageable page) {
+        logger.warn("Custom pageable");
+        return ResponseEntity.ok(repository.findAll(page).getContent());
+    }
+
+    @GetMapping("/tasks/{id}")
+    ResponseEntity<Task> readTask(@PathVariable int id) {
+        return repository.findById(id)
+                .map(ResponseEntity::ok  )
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("tasks")
+    ResponseEntity<Task> createTask(@RequestBody @Valid Task toCreate) {
+        Task result = repository.save(toCreate);
+        return ResponseEntity.created(URI.create("/" + result.getId())).body(result);
+    }
+
+    @PutMapping("/tasks/{id}")
+    ResponseEntity<?> updateTask(@PathVariable int id, @RequestBody @Valid Task toUpdate) {
+        if (!repository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        toUpdate.setId(id);
+        repository.save(toUpdate);
+        return ResponseEntity.noContent().build();
     }
 }
---
+++ src/main/java/io/github/iza/todoapp/model/SqlTaskRepository.java
@@ -0,0 +1,8 @@
+package io.github.iza.todoapp.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+interface SqlTaskRepository extends TaskRepository, JpaRepository<Task, Integer> {
+}
