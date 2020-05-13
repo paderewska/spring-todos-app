cddf550853d79fa229bdfae0894cb16ab74a5e97
Izabela Paderewska <izabela.paderewska@hycom.pl>
2020-05-13 16:32:43 +0200

    queries improved

--- src/main/java/io/github/iza/todoapp/adapter/SqlProjectRepository.java
+++ src/main/java/io/github/iza/todoapp/adapter/SqlProjectRepository.java
@@ -12,6 +12,6 @@ import java.util.List;
 public interface SqlProjectRepository extends ProjectRepository, JpaRepository<Project, Integer> {
 
     @Override
-    @Query("from Project p join fetch p.steps")
+    @Query("select distinct p from Project p join fetch p.steps")
     List<Project> findAll();
 }
