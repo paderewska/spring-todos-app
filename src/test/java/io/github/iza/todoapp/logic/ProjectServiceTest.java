9cafe5a1596f104503e9302aa843887b9dfd0b65
Izabela Paderewska <izabela.paderewska@hycom.pl>
2020-06-26 16:47:12 +0200

    Unit tests cd

--- src/test/java/io/github/iza/todoapp/logic/ProjectServiceTest.java
+++ src/test/java/io/github/iza/todoapp/logic/ProjectServiceTest.java
@@ -1,11 +1,13 @@
 package io.github.iza.todoapp.logic;
 
 import io.github.iza.todoapp.TaskConfigurationProperties;
+import io.github.iza.todoapp.model.ProjectRepository;
 import io.github.iza.todoapp.model.TaskGroupRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,10 +23,7 @@ class ProjectServiceTest {
         //given
         var mockGroupRepository = mock(TaskGroupRepository.class);
         when(mockGroupRepository.existsByDoneIsFalseAndProject_Id(anyInt())).thenReturn(true);
-        var mockTemplate = mock(TaskConfigurationProperties.Template.class);
-        when(mockTemplate.isAllowMultipleTasks()).thenReturn(false);
-        var mockTaskConfigurationProperties = mock(TaskConfigurationProperties.class);
-        when(mockTaskConfigurationProperties.getTemplate()).thenReturn(mockTemplate);
+        TaskConfigurationProperties mockTaskConfigurationProperties = configurationReturning(false);
         //system under test
         var toTest = new ProjectService(null, mockGroupRepository, mockTaskConfigurationProperties);
 
@@ -35,5 +34,33 @@ class ProjectServiceTest {
         assertThat(exception)
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessageContaining("one undone group");
+    }
+
+    @Test
+    @DisplayName("should throw IllegalArgumentException when configuration ok and no projects for a given id")
+    void createGroup_configurationOk_And_noProjects_throwsIllegalArgumentException() {
+        //given
+        var mockRepository = mock(ProjectRepository.class);
+        when(mockRepository.findById(anyInt())).thenReturn(Optional.empty());
+        //and
+        TaskConfigurationProperties mockTaskConfigurationProperties = configurationReturning(true);
+        //system under test
+        var toTest = new ProjectService(mockRepository, null, mockTaskConfigurationProperties);
+
+        //when
+        var exception = catchThrowable(() -> toTest.createGroup(LocalDateTime.now(), 0));
+
+        //then
+        assertThat(exception)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("id not found");
+    }
+
+    private TaskConfigurationProperties configurationReturning(final boolean result) {
+        var mockTemplate = mock(TaskConfigurationProperties.Template.class);
+        when(mockTemplate.isAllowMultipleTasks()).thenReturn(result);
+        var mockTaskConfigurationProperties = mock(TaskConfigurationProperties.class);
+        when(mockTaskConfigurationProperties.getTemplate()).thenReturn(mockTemplate);
+        return mockTaskConfigurationProperties;
     }
 }
